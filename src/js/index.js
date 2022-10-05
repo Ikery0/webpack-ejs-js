9dbc542dc34836ab51e42f6c2cece45967e78aa6
ikeryo <sruyrof@icloud.com>
2022-10-05 17:12:56 +0900

    done adjustment

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,17 @@
-'use strict'
-import { FitHeight } from './_utils/_fitHeight.js'
-import { Drawer } from './_utils/_drawer.js'
-import { ToggleSlide } from './_utils/_toggleSlide.js'
-import { OtherPageSmoothScroll } from './_utils/_smoothScroll.js'
+'use strict';
+import { FitHeight } from './_utils/_fitHeight.js';
+import { Drawer } from './_utils/_drawer.js';
+import { ToggleSlide } from './_utils/_toggleSlide.js';
+import { OtherPageSmoothScroll } from './_utils/_smoothScroll.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const drawerObj = {
+  new FitHeight();
+  new Drawer({
     hamburger: 'js-hamburger',
     menu: 'js-spNav',
     CLASS: 'is-active',
-    focusTrap: "js-navTrap"
-  }
-  const smoothOptions = {
-    speed: 600,
-    easing: 'easeOut'
-  }
-
-  new FitHeight()
-  new Drawer(drawerObj)
-  new ToggleSlide({ triggers: '.js-accoTrigger', targets: '.js-accoTarget'});
+    focusTrap: 'js-navTrap',
+  });
+  new ToggleSlide({ triggers: '.js-accoTrigger', targets: '.js-accoTarget' });
   new OtherPageSmoothScroll();
-})
+});
