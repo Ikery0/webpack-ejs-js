7686852962922835c106d62d5ff19baea6ff0088
ikeryo <sruyrof@icloud.com>
2022-11-12 12:33:43 +0900

    focusが背景に行かないように修正

--- src/js/_utils/_drawer.js
+++ src/js/_utils/_drawer.js
@@ -1,5 +1,6 @@
 'use strict';
 import { BgFix } from './_bgFix';
+import { modalFocus } from './_modalFocus';
 /*
 {
   hamburger: hamburger btn id, 
@@ -13,34 +14,29 @@ export class Drawer {
     this.DOM = {};
     this.DOM.hamburger = document.getElementById(obj.hamburger);
     this.DOM.menu = document.querySelector(`.${obj.menu}`);
-    this.DOM.focusTrap = document.getElementById(obj.focusTrap);
+    // this.DOM.focusTrap = document.getElementById(obj.focusTrap);
     this.CLASS = obj.CLASS;
     this.flg = false;
     this.bgFix = new BgFix();
+    this.focusHandle = (event) => modalFocus(event, this.DOM.menu);
 
     this._addEvent();
   }
 
   _addEvent() {
-    const that = this;
     this.DOM.hamburger.addEventListener('click', () => {
       //ハンバーガーボタンが選択されたら
-      that.flg ? that._close() : that._open();
-    });
-
-    this.DOM.focusTrap.addEventListener('focus', () => {
-      that.DOM.hamburger.focus();
+      this.flg ? this._close() : this._open();
     });
 
     window.addEventListener('keydown', (e) => {
-      //escキー押下でメニューを閉じられるように
       if (e.key === 'Escape') {
-        that._close();
+        this._close();
       }
     });
 
     window.addEventListener('resize', () => {
-      if (window.matchMedia('(min-width: 768px)').matches && that.flg) that._close();
+      if (window.matchMedia('(min-width: 768px)').matches && that.flg) this._close();
     });
   }
 
@@ -50,8 +46,10 @@ export class Drawer {
     this.DOM.menu.classList.add(this.CLASS);
     this.DOM.hamburger.classList.add(this.CLASS);
     this.DOM.hamburger.setAttribute('aria-expanded', 'true');
+    this.DOM.hamburger.setAttribute('aria-label', 'メニューを開く');
     this.DOM.menu.setAttribute('aria-hidden', 'false');
     this.DOM.hamburger.focus();
+    window.addEventListener('keydown', this.focusHandle);
   }
 
   _close() {
@@ -59,7 +57,9 @@ export class Drawer {
     this.bgFix.off();
     this.DOM.menu.classList.remove(this.CLASS);
     this.DOM.hamburger.classList.remove(this.CLASS);
+    this.DOM.hamburger.setAttribute('aria-label', 'メニューを開く');
     this.DOM.hamburger.setAttribute('aria-expanded', 'false');
     this.DOM.menu.setAttribute('aria-hidden', 'true');
+    window.removeEventListener('keydown', this.focusHandle);
   }
 }
